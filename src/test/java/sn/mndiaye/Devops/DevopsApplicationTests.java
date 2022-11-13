2f58aa23749b2942afef87300f78165f820cc861
Mister-Ndiaye <61950912+Mister-Ndiaye@users.noreply.github.com>
2022-11-13 18:56:22 +0000

    init

---
+++ src/test/java/sn/mndiaye/Devops/DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.mndiaye.Devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
